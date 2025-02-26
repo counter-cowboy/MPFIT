b12497cd478abba046828deb69f4314de38b819a
counter-cowboy <79208562003@ya.ru>
2025-02-26 17:49:18 +0300

    doc: products categories completed

--- resources/views/category/edit.blade.php
+++ resources/views/category/edit.blade.php
@@ -4,20 +4,19 @@
         <form action="{{ route('categories.update', $category) }}" method="post">
             @csrf
             @method('patch')
+
             <div class="mb-3">
                 <label for="exampleFormControlInput1" class="form-label">Category name</label>
                 <input type="text" class="form-control" name="title" value="{{old('title', $category->title)}}"
                        id="exampleFormControlInput1" placeholder="input product name">
+
                 @error('title')
                 <div class="text-danger">
                     Field must be fullfilled
                 </div>
                 @enderror
+
             </div>
-
-
-
-
             <button type="submit" class="btn btn-success">Update</button>
         </form>
     </div>
